056123bcea18534d6562c424d91a31a32fbf3cb7
tjeerddie <tjeerdverschragen@hotmail.com>
2020-11-03 15:32:20 +0100

    PB-4: Add Media profile model to portfolio admin
    - add media profile to admin site.
    - add migration.

--- portfolio_admin/admin/__init__.py
+++ portfolio_admin/admin/__init__.py
@@ -1,3 +1,4 @@
 from .portfolio import PortfolioAdmin
 from .project import ProjectAdmin
 from .skill import SkillAdmin
+from .media_profile import MediaProfileAdmin
--- portfolio_admin/models/__init__.py
+++ portfolio_admin/models/__init__.py
@@ -1,3 +1,4 @@
 from .portfolio import Portfolio
 from .project import Project
 from .skill import Skill
+from .media_profile import MediaProfile
